063723b397f3d200cb25f14d07ca05313f71f77d
Stephen <stephen.lyssy@gmail.com>
2022-05-20 07:06:59 -0500

    Updated styling and changed link on index.html so that it points to the contact2 page.

--- script/contact-script.js
+++ script/contact-script.js
@@ -4,7 +4,7 @@ const placeForResults = document.getElementById('results');
 new URLSearchParams(window.location.search).forEach((value, name) => {
   placeForResults.insertAdjacentHTML(
     'afterbegin',
-    `<p class=result-info><span class=name-result>${name}</span> : <span class=value-result>${value}</span></p>`
+    `<p class=result-info><span class=name-result>${name}</span> :<br> <span class=value-result>${value}</span></p>`
   );
   // placeForResults.append(`${name} : ${value}`);
   // placeForResults.append(document.createElement('br'));
